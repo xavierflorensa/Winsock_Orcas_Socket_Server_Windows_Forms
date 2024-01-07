7b8cf9569f6a3ca23e6e92a87f0af5d6d90442da
xavierflorensa <xavier.florensa@yahoo.com>
2024-01-07 09:27:02 +0100

    First TCP Server Socket version using Forms and Winsock_Orcas.dll

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Socket_test1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.winsock_Ear = new Winsock_Orcas.Winsock();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // winsock_Ear
+            // 
+            this.winsock_Ear.BufferSize = 8192;
+            this.winsock_Ear.LegacySupport = true;
+            this.winsock_Ear.LocalPort = 8080;
+            this.winsock_Ear.MaxPendingConnections = 1;
+            this.winsock_Ear.Protocol = Winsock_Orcas.WinsockProtocol.Tcp;
+            this.winsock_Ear.RemoteHost = "localhost";
+            this.winsock_Ear.RemotePort = 8080;
+            this.winsock_Ear.Connected += new Winsock_Orcas.IWinsock.ConnectedEventHandler(this.winsock_Ear_Connected);
+            this.winsock_Ear.ConnectionRequest += new Winsock_Orcas.IWinsock.ConnectionRequestEventHandler(this.winsock_Ear_ConnectionRequest);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(544, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(344, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Connection Status";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(145, 261);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Text to send";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(314, 249);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(564, 231);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 67);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(229, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Socket TCP Server: Node-RED";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(195, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Socket TCP Client: Laptop";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 375);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Winsock_Orcas.Winsock winsock_Ear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Socket_test1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //winsock_Ear.Listen(2000);//This is to make the PC act as host
+            winsock_Ear.Connect("192.168.1.163", 2000); //This is to make the PC act as client
+
+        }
+
+        private void winsock_Ear_Connected(object sender, Winsock_Orcas.WinsockConnectedEventArgs e)
+        {
+            label1.Text = "¡¡Connected to Node-RED server!!";
+        }
+
+        private void winsock_Ear_ConnectionRequest(object sender, Winsock_Orcas.WinsockConnectionRequestEventArgs e)
+        {
+            winsock_Ear.Close();
+            winsock_Ear.Accept(e.Client);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //we get the text from the Textbox entered from the keyboard 
+            string text_to_send = this.textBox1.Text;
+            winsock_Ear.Send(text_to_send);
+        }
+    }
+}
